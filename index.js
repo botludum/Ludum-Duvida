98f9353b7b8d1669522576dc0cc1831ad6b72402
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-04-23 17:15:58 -0300

    Corrigindo método post de PostSO

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
-// FileName: index.js
-
 // Import express
-let express = require('express')
-
+let express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
 // Initialize the app
 let app = express();
 
-// Setup server port
-var port = process.env.PORT || 8080;
-
 // Import routes
-let apiRoutes = require("./routes/api-routes");
-
-// Use Api routes in the App
-app.use('/api', apiRoutes);
-
-// Send message for default URL
-app.get('/', (req, res) => res.send('Hello World'));
-
-// Import Body parser
-let bodyParser = require('body-parser');
-
-// Import Mongoose
-let mongoose = require('mongoose');
+let apiRoutes = require("./routes/api-routes")
 
 // Configure bodyparser to handle post requests
 app.use(bodyParser.urlencoded({
-   extended: true
+    extended: true
 }));
-
 app.use(bodyParser.json());
 
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/8080');
+mongoose.connect('mongodb://localhost/ludum-duvida');
 
 var db = mongoose.connection;
+// Setup server port
+var port = process.env.PORT || 8080;
 
+// Send message for default URL
+app.get('/', (req, res) => res.send('Ludum Dúvida'));
+
+// Use Api routes in the App
+app.use('/api', apiRoutes)
+
+// Launch app to listen to specified port
 app.listen(port, function () {
-     console.log("Running on port " + port);
+    console.log("Running on port " + port);
 });
